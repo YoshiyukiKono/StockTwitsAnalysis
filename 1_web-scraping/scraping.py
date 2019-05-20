f10d71194fe6500ecb314a11169a8245b9500548
YoshiyukiKono <yoshiyuki.kono@gmail.com>
2019-05-20 18:39:09 +0900

    Update scraping.py

--- 1_web-scraping/scraping.py
+++ 1_web-scraping/scraping.py
@@ -1,6 +1,22 @@
 import subprocess
-args = ['curl', '-X', 'GET', 'https://api.stocktwits.com/api/2/streams/symbol/AAPL.json']
-try:
-    res = subprocess.check_call(args)
-except:
-    print "Error."
+
+symbols = ['BBRY', 'AAPL', 'AMZN', 'BABA', 'YHOO', 'LQMT', 'FB', 'GOOG', 'BBBY', 'JNUG', 'SBUX', 'MU']
+
+args = ['curl', '-X', 'GET', '']
+URL = "https://api.stocktwits.com/api/2/streams/symbol/"
+
+FILE_PATH = "./data/"
+
+for symbol in symbols:
+  try:
+    args[3] = URL + symbol + ".json"
+    print(args[3])
+    proc = subprocess.run(args,stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    
+    path = FILE_PATH + symbol + ".json"
+    
+    with open(path, mode='w') as f:
+      f.write(proc.stdout.decode("utf8"))
+  
+  except:
+    print("Error.")
