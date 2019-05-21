c73e759f5c81fd0f25e2f7ab74b76d3de6e8454f
YoshiyukiKono <yoshiyuki.kono@gmail.com>
2019-05-21 12:46:51 +0900

    Update scraping.py

--- 1_web-scraping/scraping.py
+++ 1_web-scraping/scraping.py
@@ -1,4 +1,5 @@
 import subprocess
+from datetime import datetime
 
 symbols = ['BBRY', 'AAPL', 'AMZN', 'BABA', 'YHOO', 'LQMT', 'FB', 'GOOG', 'BBBY', 'JNUG', 'SBUX', 'MU']
 
@@ -7,16 +8,20 @@ URL = "https://api.stocktwits.com/api/2/streams/symbol/"
 
 FILE_PATH = "./data/"
 
+start_datetime = datetime.now().strftime("%Y%m%d_%H%M")
+
 for symbol in symbols:
   try:
     args[3] = URL + symbol + ".json"
     print(args[3])
     proc = subprocess.run(args,stdout = subprocess.PIPE, stderr = subprocess.PIPE)
     
-    path = FILE_PATH + symbol + ".json"
+    path = FILE_PATH + symbol + "_" + start_datetime + ".json"
     
     with open(path, mode='w') as f:
       f.write(proc.stdout.decode("utf8"))
   
   except:
+    import traceback
+    traceback.print_exc()
     print("Error.")
